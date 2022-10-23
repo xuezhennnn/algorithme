e5d1d0e93968ddd148063ed9a46a6539d3a53a38
xuezhennnn <88291141+xuezhennnn@users.noreply.github.com>
2022-10-24 01:48:48 +0900

    Update 4-62_different_paths.cpp

--- Dynamic/4-62_different_paths.cpp
+++ Dynamic/4-62_different_paths.cpp
@@ -2,6 +2,12 @@
     1.创建一个二维表格的dp
     2.把所有dp[i,0] dp[0,j] 都初始化为1
     3.dp[i,j-1]+dp[i-1,j] = dp[i,j]
+    
+    debug 日记：
+    把每一个空位赋值的时候使用的是其他两个变量p q，但是程序并不能运行
+    所以变量要始终如一
+
+    二维vector的创建和初始化 vector<vector<int>> dp(m,vector<int>(n,0));
 */
 
 class Solution {
